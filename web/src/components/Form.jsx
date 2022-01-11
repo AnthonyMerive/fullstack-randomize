72dc52c68e319a31670a11d7c9b3ce99bc68251b
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2022-01-11 12:56:39 -0500

    dando algunos estilos

--- web/src/components/Form.jsx
+++ web/src/components/Form.jsx
@@ -6,10 +6,10 @@ export const Form = () => {
 
     const [state, setState] = useState(null);
     const dispatch = useDispatch();
-
+ 
     const onSubmit = (e) => {
         e.preventDefault();
-        dispatch(fetchRandom(state))
+        dispatch(fetchRandom(state));
     };
 
     return (
@@ -17,11 +17,11 @@ export const Form = () => {
             <form onSubmit={onSubmit}>
                 <label htmlFor="list">Ingrese una lista separada por comas:</label>
                 <br />
-                <textarea id="list" style={{ width: "300px", height: "120px" }}
+                <textarea className='mt-2' id="list" style={{ width: "300px", height: "120px" }}
                     onChange={(e) => setState(e.target.value)}
                 ></textarea>
                 <br />
-                <button type="submit">
+                <button type="submit" className='btn btn-primary shadow-sm'>
                     Enviar
                 </button>
             </form>
--- web/src/components/Result.jsx
+++ web/src/components/Result.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const Result = () => {
+    const result = useSelector(store => store.random.result.randomList);
     return (
-        <div>
-            resultado
+        <div className='mt-4'>
+            <h3 className='mt-2'>Resultado:</h3>
+            <p>{result}</p>
         </div>
     )
 }
